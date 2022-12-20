b3604bed7ef50a51e0cadbeaee2ba20cb0b963ae
Michael Ge <54512113+michaelge21@users.noreply.github.com>
2022-12-20 15:26:20 -0600

    key changes: font, js

--- about/main.js
+++ about/main.js
@@ -1,6 +1,7 @@
 const body = document.body;
     
-const listItems  = document.getElementsByTagName('li');
+
+const listItems  = document.getElementsByClassName('bio');
 
 const para = document.getElementById("para");
 
@@ -17,31 +18,44 @@ function changeText(item) {
     let className = item.className;
 
     switch (className) {
-        case 'shortest':             
-        para.textContent = "this is the shortest bio"
+        case 'shortest bio':             
+        para.textContent = "michael is trying his hardest."
+        para.style.fontSize = "calc(1rem + 4vw)";
+
         item.style.color = "#FF7501";
         break;
 
-        case 'short':             
-        para.textContent = "shorter bio"
+        case 'short bio':             
+        para.textContent = "Michael is a free-lance web developer passionate about defining and finding impactful work. He is trying his hardest to develop a tolerance to spicy food."
+        item.style.color = "#FF7501";
+        para.style.lineHeight = "calc(3rem - 1vh + 1.8vw)";
+
+        para.style.fontSize = "calc(1rem + 3vw)";
+
+        break;
+
+        case 'middle bio':
+        para.textContent = "Michael is a multi-disciplinary developer, drawing upon backgrounds in cognitive and educational psych, creative writing, and teaching. A proud generalist, he tries his hardest to make something better everyday."
+        para.style.fontSize = "calc(1rem + 2.5vw)";
+        para.style.lineHeight = "calc(3rem - 1vh + 1.5vw)";
+
         item.style.color = "#FF7501";
         break;
 
-        case 'middle':
-        para.textContent = "meeeeedium length!"
-        item.style.color = "#FF7501";
-        break;
-
-        case 'longer':
-            para.textContent = "long, long, long, longer!"
+        case 'longer bio':
+            para.textContent = "Michael chases abstract terms like “impact,” “change,” and “leadership,” spending late nights mulling over how to operationalize and apply them to his work. He built a pan-Asian mental health conference as an undergrad, and continues to speak and advocate for his communities."
             item.style.color = "#FF7501";
+            para.style.fontSize = "calc(1rem + 2vw)";
+            para.style.lineHeight = "calc(3rem - 1vh + 1.1vw)";
             break;
 
-        case 'longest':
-        para.textContent = "long long length! long long length!long long length!long long length!long long length!long long length!long long length!long long length!"
+        case 'longest bio':
+        para.textContent = 'michael is a second-gen Chinese American raised in Wisconsin. The last five years have been a journey exploring being Asian in the Midwest, and he has firmly settled on the answer of: “I go out for Culver\'s, but I go home for 红烧肉 (red braised pork).” When his joints do not ache, he teaches breaking and house in Madison. He leads teams on the belief that friends achieve more than coworkers. Michael will never stop supporting Liverpool FC unless someone confronts him confrontationally.'
+        para.style.fontSize = "calc(.5rem + 2vw)"
+        para.style.lineHeight = "calc(3rem - 1vh + .8vw)";
         item.style.color = "#FF7501";
         break;
 
-        default: para.textContent = 'error! error!'
+        default: 
     }
 }
