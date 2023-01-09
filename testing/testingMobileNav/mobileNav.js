5667c433e54689a5bff66ad74b16ae9ee7840068
Michael Ge <michaelge@wustl.edu>
2023-01-09 13:34:44 -0600

    beta mobile nav.
    color sync and microint required

--- testing/testingMobileNav/mobileNav.js
+++ testing/testingMobileNav/mobileNav.js
@@ -1,4 +1,5 @@
 const menuButton = document.querySelector(".menuButton");
+const mobileNavMenu = document.querySelector(".navButtons");
 
 menuButton.addEventListener('click', (event) => {
 
@@ -6,5 +7,8 @@ menuButton.addEventListener('click', (event) => {
     ? event.target.textContent = "X"
     : event.target.textContent = "menu";
 
+    menuButton.classList.toggle("smallDimensions")
+    mobileNavMenu.classList.toggle("placeOffScreen");
 
 });
+
