c49b7881f1031b34641f42c71d8bb991770b3677
Michael Ge <michaelge@wustl.edu>
2023-01-25 18:43:48 -0600

    using prettier

--- mobileNav.js
+++ mobileNav.js
@@ -1,26 +1,21 @@
 const bodyE = document.querySelector("body");
 
-
 const menuButton = document.querySelector(".menuButton");
 const mobileNavMenu = document.querySelector(".navButtons");
 const entireNav = document.querySelector(".mobileContainer");
 
+menuButton.addEventListener("click", (event) => {
+  event.target.textContent == "menu"
+    ? (event.target.textContent = "X")
+    : (event.target.textContent = "menu");
 
-menuButton.addEventListener('click', (event) => {
+  bodyE.style.overflow == "hidden"
+    ? bodyE.style.overflow == "unset"
+    : bodyE.style.overflow == "hidden";
 
-    (event.target.textContent == "menu")
-    ? event.target.textContent = "X"
-    : event.target.textContent = "menu";
+  menuButton.classList.toggle("smallDimensions");
+  mobileNavMenu.classList.toggle("placeOffScreen");
+  bodyE.classList.toggle("noOverflow");
 
-    (bodyE.style.overflow == "hidden")
-    ? bodyE.style.overflow =="unset"
-    : bodyE.style.overflow =="hidden";
-
-    menuButton.classList.toggle("smallDimensions")
-    mobileNavMenu.classList.toggle("placeOffScreen");
-    bodyE.classList.toggle("noOverflow");
-
-    entireNav.classList.toggle("makeMobileWork");
-
+  entireNav.classList.toggle("makeMobileWork");
 });
-
